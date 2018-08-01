2ab35bd208f294989e991ce9f6148c636028a642
Wade Aston <mrwadepro@gmail.com>
2018-08-01 13:59:52 -0500

    Before adding Calendar, raw code

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="text-white mt-5 p-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} STEMuli
+    </footer>
+  );
+};
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://wade:wade231@ds249398.mlab.com:49398/stemtest",
+  secretOrKey: "secret"
+};
