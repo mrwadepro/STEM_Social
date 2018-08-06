065ca8220686e8cbb22fd0fc5796109dd6bf36df
Wade Aston <mrwadepro@gmail.com>
2018-08-06 09:07:40 -0500

    Removed incapatable profile

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -6,8 +6,6 @@ import isEmpty from "../../validation/is-empty";
 class ProfileItem extends Component {
   render() {
     const { profile } = this.props;
-    console.log(profile);
-
     return (
       <div className="card card-body bg-light mb-3">
         <div className="row">
