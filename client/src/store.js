778ce86085ee54eecce8953239e95c05b7ed1096
Wade Aston <mrwadepro@gmail.com>
2018-08-18 16:26:01 -0500

    remove redux tools

--- client/src/store.js
+++ client/src/store.js
@@ -27,9 +27,6 @@ if (isChrome) {
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware))
 );
 export default store;
