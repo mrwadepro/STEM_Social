1d00ca8e37b464e17b44e0d4c0339b3c1c62407d
Wade Aston <mrwadepro@gmail.com>
2018-08-27 23:30:40 -0500

    removed redux for testing

--- client/src/store.js
+++ client/src/store.js
@@ -5,23 +5,10 @@ import rootReducer from "./reducers";
 const initialState = {};
 var isChrome = !!window.chrome && !!window.chrome.webstore;
 const middleware = [thunk];
-let store;
-if (isChrome) {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-} else {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware))
-  );
-}
+let store = createStore(
+  rootReducer,
+  initialState,
+  compose(applyMiddleware(...middleware))
+);
 
 export default store;
